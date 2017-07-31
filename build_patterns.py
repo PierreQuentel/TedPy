e625deb338f9916c38967b5bbce5da319b6f6e84
Pierre Quentel <quentel.pierre@orange.fr>
2017-07-31 21:52:46 +0200

    Fixes #614 : hidden extra 'length' attribute in namedtuples

--- build_patterns.py
+++ build_patterns.py
@@ -1,5 +1,5 @@
 import keyword
-out=open("_patterns.py","w")
+out = open("_patterns.py", "w")
 out.write("keywords = "+str(keyword.kwlist)+"\n")
-out.write("builtins = %s" %dir(__builtins__))
+out.write("builtins = {}".format(dir(__builtins__)))
 out.close()
