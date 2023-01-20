9ca76e9fe50e5e406ee862ccbd0d527999269d2f
PierreQuentel <pierre.quentel@gmail.com>
2023-01-20 12:49:56 +0100

    Detect Javascript iterator functions ("function*")

--- languages/js.py
+++ languages/js.py
@@ -1,6 +1,9 @@
 extensions = ['.js']
 script_types = ['text/javascript']
-struct_patterns = [r'\s*\bfunction\b', r'\s*.*=\s*function\(.*', r'\s*.*:\s*function\(.*']
+struct_patterns = [r'\s*\bfunction\*?\b',
+                   r'\s*.*=\s*function\*?\(.*',
+                   r'\s*.*:\s*function\*?\(.*'
+                   ]
 keywords = ['break','case','catch', 'const', 'continue','debugger',
     'default','delete','do','else','finally','for','function',
     'if','in','instanceof','let', 'new','return','switch','this',
