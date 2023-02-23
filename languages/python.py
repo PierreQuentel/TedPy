916f9b37afd28d033e7a0986f86542db22934a78
PierreQuentel <pierre.quentel@gmail.com>
2023-02-23 17:22:32 +0100

    Add "async def" in functions / classes browser for Python scripts

--- languages/python.py
+++ languages/python.py
@@ -7,5 +7,5 @@ zones = [ ('"""', '"""', 'string'), ("'''", "'''", 'string'),
         ('#', '\n', 'comment')]
 extensions = ['.py']
 script_types = ['text/python', 'text/python3']
-struct_patterns = [r'\s*\bdef\b', r'\s*\bclass\b']
+struct_patterns = [r'\s*\b(async\s+)?def\b', r'\s*\bclass\b']
 autoindent_lineend = ':'
